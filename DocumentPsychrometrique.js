a5b4dd76c50f0991f357e35753b558a8ca6195a8
jbsky <webmaster@jbsky.fr>
2018-04-02 19:37:29 +0200

    Add files via upload

--- DocumentPsychrometrique.js
+++ DocumentPsychrometrique.js
@@ -10,6 +10,11 @@ function DocumentPsychrometrique(Document) {
 
     var OptionMassMolaire = false;
 
+    var divListPoint ;
+    this.setListPoint = function(div){
+        divListPoint = Document.getElementById(div);
+    }
+
     this.writeOptionMassMolaire = function (div) {
         OptionMassMolaire = true;
         var theDiv = Document.getElementById(div);
@@ -51,76 +56,52 @@ function DocumentPsychrometrique(Document) {
 
     this.writeCalculateur = function (div) {
         var theDiv = Document.getElementById(div);
-        var content = '<h1>Paramètre Psychrométrique</h1>\
-        <div class="align">\
-        <div >\
-        <select name="option" id="option">\
-        <option value="R">Humidité Absolue</option>\
-        <option value="HR">Humidité Relative</option>\
-        </select></div>\
-        <div id="dim"><input type="button" onclick="DocumentPsychro.Calc();" value="Calc"></input></div>\
-        <div>\
-        <div></div>\
-        </div>\
-        </div>\
-        <div >\
-        <div class="align" >\
-        <div>Pression atmosphérique</div>\
-        <div>[Pa]</div>\
-        <div><input type="text" id="Pression" value="101325"  readonly="true" class="right"></input></div>\
-        </div>\
-        <div class="align">\
-        <div>Altitude</div>\
-        <div>[m]</div>\
-        <div><input type="text" id="Altitude" value="0" class="right"></input></div>\
-        </div>\
-        <div class="align">\
-        <div>Temperature du bulbe sec</div>\
-        <div>[C]</div>\
-        <div><input type="text" id="Tsec" value="25" class="right"></input></div>\
-        </div>\
-        <div class="align">\
-        <div>Pression de vapeur saturante</div>\
-        <div>[Pa]</div>\
-        <div><input type="text" id="Pvs" value="" readonly="true" class="right"></input></div>\
-        </div>\
-        <div class="align">\
-        <div>Pression partielle de vapeur</div>\
-        <div>[Pa]</div>\
-        <div><input type="text" id="Pv" value="" readonly="true" class="right"></input></div>\
-        </div>\
-        <div class="align">\
-        <div>Temperature du bulbe humide</div>\
-        <div>[C]</div>\
-        <div><input type="text" id="Th" value="" readonly="true" class="right"></input></div>\
-        </div>\
-        <div class="align">\
-        <div>Point de rosée</div>\
-        <div>[C]</div>\
-        <div><input type="text" id="Tr" readonly="true" class="right"></input></div>\
-        </div>\
-        <div class="align">\
-        <div>Humidité Relative</div>\
-        <div>[%]</div>\
-        <div><input type="text" id="HR" value="50" pattern="[0-9]{.}" class="right"></input></div>\
-        </div>\
-        <div class="align">\
-        <div>Humidité spécifique</div>\
-        <div>[kgH2O/kgAIR]</div>\
-        <div><input type="text" id="R" value="0.010" pattern="[0-9]{.}" class="right"></input></div>\
-        </div>\
-        <div class="align">\
-        <div>Enthalpy de l\'air sec</div>\
-                        <div>[kJ/kg]</div>\
-                        <div><input type="text" id="Enthalpie" readonly="true" class="right"></input></div>\
-                    </div>\
-                    <div class="align">\
-                        <div>Volume spécifique</div>\
-                        <div>[Kg/m3]</div>\
-                        <div><input type="text" id="Vs" readonly="true" class="right"></input></div>\
-                    </div>\
-                </div>';
+        var content = '<div>\
+            <div class="align"><div class="dimension">Pression atmosphérique</div><div class="dimension2">[Pa]</div><div><input type="text" id="Pression" value="101325"  readonly="true" class="right"></input></div></div>\
+            <div class="align"><div class="dimension">Altitude</div><div class="dimension2">[m]</div><div><input type="text" id="Altitude" value="0" class="right"></input></div></div>\
+            <div class="align"><div class="dimension">Temperature du bulbe sec</div><div class="dimension2">[C]</div><div><input type="text" id="Tsec" value="25" class="right"></input></div></div>\
+            <div class="align"><div class="dimension">Pression de vapeur saturante</div><div class="dimension2">[Pa]</div><div><input type="text" id="Pvs" value="" readonly="true" class="right"></input></div></div>\
+            <div class="align"><div class="dimension">Pression partielle de vapeur</div><div class="dimension2">[Pa]</div><div><input type="text" id="Pv" value="" readonly="true" class="right"></input></div></div>\
+            <div class="align"><div class="dimension">Temperature du bulbe humide</div><div class="dimension2">[C]</div><div><input type="text" id="Th" value="" readonly="true" class="right"></input></div></div>\
+            <div class="align"><div class="dimension">Point de rosée</div><div class="dimension2">[C]</div><div><input type="text" id="Tr" readonly="true" class="right"></input></div></div>\
+            <div class="align"><div class="dimension">Humidité Relative</div><div class="dimension2">[%]</div><div><input type="text" id="HR" value="50" pattern="[0-9]{.}" class="right"></input></div></div>\
+            <div class="align"><div class="dimension">Humidité spécifique</div><div class="dimension2">[kgH2O/kgAIR]</div><div><input type="text" id="R" value="0.010" pattern="[0-9]{.}" class="right"></input></div></div>\
+            <div class="align"><div class="dimension">Enthalpy de l\'air sec</div><div class="dimension2">[kJ/kg]</div><div><input type="text" id="Enthalpie" readonly="true" class="right"></input></div></div>\
+            <div class="align"><div class="dimension">Volume spécifique</div><div class="dimension2">[Kg/m3]</div><div><input type="text" id="Vs" readonly="true" class="right"></input></div></div>\
+            <BR>\
+            <div class="align"><div><select class="select" name="option" id="option"><option value="R">Humidité Absolue</option><option value="HR">Humidité Relative</option></select></div><div></div><div><input type="button" onclick="DocumentPsychro.AddPoint(\'ListPoint\');" value="Add Point" class="button"></input></div></div></div>';
         theDiv.innerHTML = content;
+    }
+    this.AddPoint = function () {
+        this.Psychro.setTsec(Document.getElementById("Tsec").value);
+        this.Psychro.setAltitude(Document.getElementById("Altitude").value);
+        switch (Document.getElementById("option").value) {
+            case "HR":
+                this.Psychro.setHR(Document.getElementById("HR").value);
+                break;
+            case "R":
+                this.Psychro.setR(Document.getElementById("R").value);
+                break;
+        }
+        this.Psychro.calc();
+        var  newpoint=  new point(parseFloat(this.Psychro.Tsec), parseFloat(this.Psychro.R) * 1000);
+
+        if(this.Graph.context!== undefined)
+            this.Graph.drawPointGraph(newpoint, '#4eff00', 25);
+        var content = '<div >\
+        <div class="align"><div><input type="text" id="Pression" value="'+ String(parseFloat(this.Psychro.Pression).toFixed(0)) +'"  readonly="true" class="right"> </input></div></div>\
+        <div class="align"><div><input type="text" id="Altitude" value="'+ String(parseFloat(this.Psychro.Altitude).toFixed(2)) +'" readonly="true" class="right">  </input></div></div>\
+        <div class="align"><div><input type="text" id="Tsec" value="'+ String(parseFloat(this.Psychro.Tsec).toFixed(2)) +'" readonly="true" class="right">          </input></div></div>\
+        <div class="align"><div><input type="text" id="Pvs" value="'+ String(parseFloat(this.Psychro.Pvs).toFixed(2)) +'" readonly="true" class="right">            </input></div></div>\
+        <div class="align"><div><input type="text" id="Pv" value="'+ String(parseFloat(this.Psychro.Pv).toFixed(2)) +'" readonly="true" class="right">              </input></div></div>\
+        <div class="align"><div><input type="text" id="Th" value="'+ String(parseFloat(this.Psychro.Th).toFixed(2)) +'" readonly="true" class="right">              </input></div></div>\
+        <div class="align"><div><input type="text" id="Tr" value="'+ String(parseFloat(this.Psychro.Tr).toFixed(2)) +'"readonly="true" class="right">               </input></div></div>\
+        <div class="align"><div><input type="text" id="HR" value="'+ String(parseFloat(this.Psychro.HR).toFixed(2)) +'" readonly="true"  class="right">             </input></div></div>\
+        <div class="align"><div><input type="text" id="R" value="'+ String(parseFloat(this.Psychro.R).toFixed(4)) +'" readonly="true"  class="right">               </input></div></div>\
+        <div class="align"><div><input type="text" id="Enthalpie" value="'+ String(parseFloat(this.Psychro.Enthalpie).toFixed(2)) +'" readonly="true" class="right"></input></div></div>\
+        <div class="align"><div><input type="text" id="Vs" value="'+ String(parseFloat(this.Psychro.Vs).toFixed(2)) +'" readonly="true" class="right">              </input></div></div>\
+        </div>';
+        divListPoint.innerHTML += content;
     }
 
     var readFile = function (file) {
@@ -157,9 +138,9 @@ function DocumentPsychrometrique(Document) {
         Document.getElementById("Tr").value = parseFloat(this.Psychro.Tr).toFixed(2);
         Document.getElementById("Th").value = parseFloat(this.Psychro.Th).toFixed(2);
         Document.getElementById("Vs").value = parseFloat(this.Psychro.Vs).toFixed(2);
-       var  newpoint=  new point(parseFloat(this.Psychro.Tsec), parseFloat(this.Psychro.R) * 1000);
-       if(this.Graph.context!== undefined)
-            this.Graph.drawPointGraph(newpoint, '#ff00ca', 50);
+        var  newpoint=  new point(parseFloat(this.Psychro.Tsec), parseFloat(this.Psychro.R) * 1000);
+        if(this.Graph.context!== undefined)
+            this.Graph.drawPointGraph(newpoint, '#4eff00', 25);
 
 
     }
@@ -182,7 +163,7 @@ function DocumentPsychrometrique(Document) {
         var res = readFile.call(this, newPathname + 'psychrometrique.json');
         var b = JSON.parse(res);
         var R = this.Psychro.calcR(parseFloat(b.Humidite_Relative), parseFloat(b.Temperature), parseFloat(b.Pression * 100));
-        this.Graph.drawPointGraph(new point(b.Temperature, R * 1000), '#ff00ca', 50);
+        this.Graph.drawPointGraph(new point(b.Temperature, R * 1000), '#001fff', 25);
     }
 
     this.OnClick = function (e) {
@@ -192,6 +173,6 @@ function DocumentPsychrometrique(Document) {
         Document.getElementById("Tsec").value = parseFloat(rPoint.x);
         Document.getElementById("R").value = parseFloat(rPoint.y) / 1000;
         this.Calc();
-        this.Graph.drawPointGraph(rPoint, '#FF0000', 20);
+        this.Graph.drawPointGraph(rPoint, '#FF0000', 25);
     }
 }
